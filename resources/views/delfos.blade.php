74ba0360c07cffba9ceb40cca2f770ba4cda73b8
tsalmeida <tsilvaalmeida@gmail.com>
2023-11-29 22:01:42 +0800

    Working

--- resources/views/delfos.blade.php
+++ resources/views/delfos.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <div class="container">
     <x-white-board>
-        <h1>Plataforma Delos</h1>
+        <h1>Plataforma Delfos</h1>
 
-        <p>A Delos é um sistema avançado de geração de simulados feitos sob medida para cada aluno etc. etc. etc.</p>
+        <p>A Delfos é um sistema avançado de geração de simulados feitos sob medida para cada aluno etc. etc. etc.</p>
     </x-white-board>
 </div>
 
