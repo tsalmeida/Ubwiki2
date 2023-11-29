6dc4d8014c120e68c92845afb1bd7010c155e20c
tsalmeida <tsilvaalmeida@gmail.com>
2023-11-29 20:50:36 +0800

    Working

---
+++ resources/views/delos.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>Plataforma Delos</h1>
+
+    <p>A Delos é um sistema avançado de geração de simulados feitos sob medida para cada aluno etc. etc. etc.</p>
+
+@endsection
