f63fde1e14b6a81199485e821cddc20969bd20fb
tsalmeida <tsilvaalmeida@gmail.com>
2023-11-29 08:34:29 +0800

    Working

--- laravel/resources/views/admin.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- Correct usage --}}
-@extends('layout')
-
-@section('content')
-    <div class="container">
-        <h1 class="text-center">{{ __('messages.admin_page') }}</h1>
-        <!-- Your admin page content -->
-    </div>
-@endsection
--- laravel/resources/views/components/icon-button.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="{{ $link }}" classes="{{ $classes }}" class="icon-button" title="{{ $title }}">
-    <i class="{{ $icon }}"></i>
-</a>
