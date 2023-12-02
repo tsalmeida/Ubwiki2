80583b05f90ebc3e185c7190425831968bcd7d2f
tsalmeida <tsilvaalmeida@gmail.com>
2023-12-02 14:12:28 +0800

    changes

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,26 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-weight-bold text-lg text-dark">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+    <div class="py-4">
+        <div class="container px-sm-3 px-md-4">
+            <div class="p-3 shadow rounded bg-white">
+                <div class="mx-auto" style="max-width: 40rem;">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="mt-4 p-3 shadow rounded bg-white">
+                <div class="mx-auto" style="max-width: 40rem;">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <div class="mt-4 p-3 shadow rounded bg-white">
+                <div class="mx-auto" style="max-width: 40rem;">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
--- resources/views/ubwiki/dashboard.blade.php
+++ resources/views/ubwiki/dashboard.blade.php
@@ -1,25 +1,25 @@
 @extends('layout')
 
 @section('content')
-<div class="container">
-    <x-white-board>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="container">
+        <x-white-board>
+            <x-app-layout>
+                <x-slot name="header">
+                    <h2 class="font-weight-bold text-lg text-dark">
+                        {{ __('Dashboard') }}
+                    </h2>
+                </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="py-4">
+                    <div class="mx-auto" style="max-width: 1200px;"> <!-- Adjusted max width -->
+                        <div class="bg-white shadow rounded">
+                            <div class="p-3 text-dark"> <!-- Text color adjusted -->
+                                {{ __("You're logged in!") }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </x-app-layout>
+        </x-white-board>
     </div>
-</x-app-layout>
-    </x-white-board>
-</div>
 @endsection
